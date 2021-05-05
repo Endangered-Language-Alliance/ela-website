e2fe3e9ca2bea8b623725ee34a8d50dcc8d035c1
Jason Lampel <jason@abettermap.com>
2021-05-05 09:40:29 -0500

    Init blog from WP example

---
+++ pages/blog/[slug].js
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+// data
+import { getAllPostsWithSlug, getPost } from '../../lib/api';
+
+// styles
+import styles from '../../styles/Home.module.css';
+import blogStyles from '../../styles/Blog.module.css';
+
+export default function Post({ postData }) {
+  if (!postData) {
+    return <p>No data could be found for the post...</p>;
+  }
+
+  const formatDate = date => {
+    const newDate = new Date(date);
+
+    return `${newDate.getDate()}/${newDate.getMonth() + 1
+      }/${newDate.getFullYear()}`;
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{postData.title}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <article className={blogStyles.article}>
+          <div className={blogStyles.postmeta}>
+            <h1 className={styles.title}>{postData.title}</h1>
+            <p>{formatDate(postData.date)}</p>
+          </div>
+          <div
+            className='post-content content'
+            dangerouslySetInnerHTML={{ __html: postData.content }}
+          />
+        </article>
+        <p>
+          <Link href='/blog'>
+            <a>back to articles</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const allPosts = await getAllPostsWithSlug();
+
+  return {
+    paths: allPosts.edges.map(({ node }) => `/blog/${node.slug}`) || [],
+    fallback: false
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getPost(params.slug);
+
+  return {
+    props: {
+      postData: data.post
+    }
+  };
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+// data
+import { getAllPosts } from '../../lib/api';
+
+// styles
+import styles from '../../styles/Home.module.css';
+import blogStyles from '../../styles/Blog.module.css';
+
+function createMarkup(text) {
+  return { __html: text };
+}
+
+const Blog = ({ allPosts: { edges } }) => (
+  <div className={styles.container}>
+    <Head>
+      <title>Blog articles page</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>Latest blog articles</h1>
+      <hr />
+      <section>
+        {edges.map(({ node }) => (
+          <div className={blogStyles.listitem} key={node.id}>
+            <div className={blogStyles.listitem__content}>
+              <h2>
+                <Link href={`/blog/${node.slug}`}>
+                  <a>{node.title}</a>
+                </Link>
+              </h2>
+              <time className={blogStyles.listitem__date} datetime={node.date}>{node.date}</time>
+              <div dangerouslySetInnerHTML={createMarkup(node.excerpt)}></div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  </div>
+);
+
+export default Blog;
+
+export async function getStaticProps() {
+  const allPosts = await getAllPosts();
+
+  return {
+    props: {
+      allPosts
+    }
+  };
+}
