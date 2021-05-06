ad00e3fab5c2ae6b855e21c4e3b1e5bd8d8842c4
Jason Lampel <jason@abettermap.com>
2021-05-06 13:18:50 -0500

    Header/nav: parent/child menus working nicely

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,7 +6,6 @@ import {
   Menu,
   MenuList,
   MenuButton,
-  MenuItem,
   // MenuItems, // TODO: understand, restore
   MenuLink,
 } from '@reach/menu-button'
@@ -17,32 +16,42 @@ import { Menu as WpMenu } from '../../wp-graphql'
 const API_URL = process.env.NEXT_PUBLIC_WP_API_URL as string
 
 const Header: FC = () => {
-  const { data, error, isLoading } = useQuery<WpMenu>('posts', async () => {
-    const mainMenuResp = await request(
-      API_URL,
-      gql`
-        query MainMenuQuery {
-          menuItems(where: { location: PRIMARY }) {
-            nodes {
-              ...MenuBits
-              childItems {
-                nodes {
-                  ...MenuBits
+  const { data, error, isLoading } = useQuery<WpMenu>(
+    'posts',
+    async () => {
+      const mainMenuResp = await request(
+        API_URL,
+        gql`
+          query MainMenuQuery {
+            menuItems(where: { location: PRIMARY, parentId: "" }) {
+              nodes {
+                ...MenuBits
+                childItems {
+                  nodes {
+                    ...MenuBits
+                  }
                 }
               }
             }
           }
-        }
 
-        fragment MenuBits on MenuItem {
-          label
-          path
-        }
-      `
-    )
+          fragment MenuBits on MenuItem {
+            label
+            path
+            parentId
+          }
+        `
+      )
 
-    return mainMenuResp
-  })
+      return mainMenuResp
+    },
+    {
+      cacheTime: Infinity,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    }
+  )
 
   if (isLoading) return <span>Loading...</span>
   if (error) return <span>Error!</span>
@@ -51,12 +60,11 @@ const Header: FC = () => {
 
   return (
     <nav>
-      <ul style={{ display: 'flex' }}>
+      <ul>
         {menuItems.nodes.map((node) => {
-          const { label, path, childItems } = node
+          const { label, path, childItems, parentId } = node
 
-          console.log(node)
-          if (!childItems.nodes.length) {
+          if (!parentId && !childItems.nodes.length) {
             return (
               <li key={path}>
                 <Link href={path}>
@@ -67,22 +75,28 @@ const Header: FC = () => {
           }
 
           return (
-            <Menu key={path}>
-              <MenuButton>{label}</MenuButton>
-              <MenuList>
-                {childItems.nodes.length ? (
-                  childItems.nodes.map((item) => {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    return <MenuItem key={item.label}>{item.label}</MenuItem>
-                  })
-                ) : (
-                  <MenuLink as={Link} to={item.path}>
-                    Somewhere w/ Reach Router
-                  </MenuLink>
-                )}
-              </MenuList>
-            </Menu>
+            <li key={path}>
+              <Menu>
+                <MenuButton>{label}</MenuButton>
+                <MenuList>
+                  <Link href={path}>
+                    <a>View all</a>
+                  </Link>
+                  {childItems.nodes.map((item) => {
+                    return (
+                      <MenuLink
+                        key={item.path}
+                        as={Link}
+                        to={item.path}
+                        href={item.path}
+                      >
+                        <a>{item.label}</a>
+                      </MenuLink>
+                    )
+                  })}
+                </MenuList>
+              </Menu>
+            </li>
           )
         })}
       </ul>
