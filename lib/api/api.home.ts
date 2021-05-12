09086a4ac26af47a0d25c438f5301b7633b51d4f
Jason Lampel <jason@abettermap.com>
2021-05-12 17:08:35 -0500

    Regular pages working

--- lib/api/api.home.ts
+++ lib/api/api.home.ts
@@ -1,7 +1,9 @@
 import { request } from 'graphql-request'
 
-import { Page, Post } from 'gql-ts/wp-graphql'
-import homePageQuery from '../../pages/HomePageQuery.graphql'
+import { Page, Post, RootQueryToPageConnection } from 'gql-ts/wp-graphql'
+import homePageQuery from 'lib/gql-queries/home/HomePageQuery.graphql'
+import publishedPagesQuery from 'lib/gql-queries/home/PublishedPages.graphql'
+import getPageQuery from 'lib/gql-queries/home/GetPage.graphql'
 
 const API_URL = process.env.WP_API_URL as string
 
@@ -11,4 +13,23 @@ export async function getHomePageContent() {
   }>(API_URL, homePageQuery)
 
   return data
+}
+
+export async function getPublishedPages(): Promise<RootQueryToPageConnection> {
+  const response = await request<{
+    data: { pages: RootQueryToPageConnection }
+  }>(API_URL, publishedPagesQuery)
+
+  return response?.data?.pages
+}
+
+export const getPage = async (
+  uri: string | string[]
+): Promise<{ page: Page }> => {
+  const data = await request(API_URL, getPageQuery, {
+    id: uri,
+    idType: 'URI',
+  })
+
+  return data?.page
 }
