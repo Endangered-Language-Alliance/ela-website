00378e6f2d08ddcbcc930c7749d739363184b8b2
Jason Lampel <jason@abettermap.com>
2021-07-18 10:44:47 -0500

    Componentize <Head>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useRef } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -14,6 +13,7 @@ import { LayoutProps } from './types'
 import { YouTubePlaylist } from './video/YouTubePlaylist'
 
 import styles from './Layout.module.css'
+import { CustomHead } from './CustomHead'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,27 +27,11 @@ const queryClient = new QueryClient({
   },
 })
 
-const SITE_NAME = 'Endangered Language Alliance'
-const SITE_URL = 'https://elalliance.org/'
-const SITE_LOGO = 'https://elalliance.org/ela-logo-hq-black.png'
-const SITE_DESC =
-  'Founded in 2010, the Endangered Language Alliance (ELA) is a non-profit dedicated to documenting Indigenous, minority, and endangered languages, supporting linguistic diversity in New York City and beyond.'
-
 export const Layout: React.FC<LayoutProps> = (props) => {
   const { title, children, summary } = props
   const { subtitle, chipsItems, noContentWrap } = props
-  const { tweenerContent, youTubePlaylistId, childPages, titleTitle } = props
-  const pageTitle = titleTitle || title || ''
-  const pageSummary = summary || SITE_DESC
+  const { tweenerContent, youTubePlaylistId, childPages } = props
 
-  let pageImgUrl = SITE_LOGO
-
-  // UGHHHHHHH operator-linebreak
-  if (childPages && childPages[0] && childPages[0].featuredImage) {
-    pageImgUrl = childPages[0].featuredImage.node?.sourceUrl || ''
-  }
-
-  const titlePrefix = pageTitle ? `${pageTitle} - ` : ''
   const targetElemID = 'back-to-top-anchor'
   const refElem = useRef<HTMLDivElement | null>(null)
 
@@ -56,27 +40,7 @@ export const Layout: React.FC<LayoutProps> = (props) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <meta name="description" content={pageSummary} />
-        <meta name="apple-mobile-web-app-title" content="ELA" />
-        <meta name="application-name" content={SITE_NAME} />
-        <meta property="og:description" content={pageSummary} key="ogdesc" />
-        <meta property="og:image" content={pageImgUrl} key="ogimg" />
-        <meta property="og:site_name" content={SITE_NAME} key="ogsitename" />
-        <meta property="og:title" content={pageTitle} key="ogtitle" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={SITE_URL} key="ogurl" />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={pageSummary} />
-        <meta name="twitter:image" content={pageImgUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-        {/* TODO: favicon, app tile */}
-        <link rel="icon" href="/favicon.ico" />
-        <title>
-          {titlePrefix}
-          {SITE_NAME}
-        </title>
-      </Head>
+      <CustomHead {...props} />
       <div className={styles.page}>
         <Header />
         <div className={styles.scrollWrap} ref={refElem}>
