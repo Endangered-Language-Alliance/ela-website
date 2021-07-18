aa1ea4f2878e6b1f2c61450e90f4181b050967fa
Jason Lampel <jason@abettermap.com>
2021-07-17 19:23:36 -0500

    Init Open Graph <meta> tags, etc.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,22 +27,53 @@ const queryClient = new QueryClient({
   },
 })
 
+const SITE_NAME = 'Endangered Language Alliance'
+const SITE_URL = 'https://elalliance.org/'
+const SITE_LOGO = 'https://elalliance.org/ela-logo-hq-black.png'
+
 export const Layout: React.FC<LayoutProps> = (props) => {
   const { title, children, summary } = props
   const { subtitle, chipsItems, noContentWrap } = props
   const { tweenerContent, youTubePlaylistId, childPages, titleTitle } = props
-  const titlePrefix = titleTitle || title ? `${titleTitle || title} - ` : ''
+  const pageTitle = titleTitle || title || ''
+  const pageSummary = summary || ''
+
+  let pageImgUrl = SITE_LOGO
+
+  // UGHHHHHHH operator-linebreak
+  if (childPages) {
+    pageImgUrl = childPages[0].featuredImage?.node?.sourceUrl || ''
+  }
+
+  const titlePrefix = pageTitle ? `${pageTitle} - ` : ''
   const targetElemID = 'back-to-top-anchor'
   const refElem = useRef<HTMLDivElement | null>(null)
+
   // High threshold since content elem might already be in view
   const hide = useHideOnScroll(refElem, 450)
 
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
-        <title>{titlePrefix}Endangered Language Alliance</title>
-        {/* TODO: meta, app-icons, social media, etc. */}
+        <meta name="description" content={pageSummary} />
+        <meta name="apple-mobile-web-app-title" content="ELA" />
+        <meta name="application-name" content={SITE_NAME} />
+        <meta property="og:description" content={pageSummary} key="ogdesc" />
+        <meta property="og:image" content={pageImgUrl} key="ogimg" />
+        <meta property="og:site_name" content={SITE_NAME} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={SITE_URL} key="ogurl" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageSummary} />
+        <meta name="twitter:image" content={pageImgUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        {/* TODO: favicon, app tile */}
         <link rel="icon" href="/favicon.ico" />
+        <title>
+          {titlePrefix}
+          {SITE_NAME}
+        </title>
       </Head>
       <div className={styles.page}>
         <Header />
